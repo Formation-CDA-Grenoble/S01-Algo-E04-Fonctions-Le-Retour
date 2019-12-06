c4eba9b8116f72df3a070b90a62160b5935067b9
Romain Tello <romain.tello@gmail.com>
2019-12-06 12:30:34 +0100

    Solve exercise 1 with bonus

--- exo1.py
+++ exo1.py
@@ -1,13 +1,6 @@
 def isInside(list, element):
     # Début de ton code
-    for item in list:
-        if item == element:
-            return True
-    # Je dois attendre la fin de la boucle pour avoir testé tous les éléments de la liste
-    # et donc pouvoir affirmer que l'élément que je cherchais ne s'y trouve pas
-    # Je dois donc retirer l'indentation pour signifier au Python qu'il faut attendre
-    # la fin de la boucle pour renvoyer False
-    return False
+    return element in list
     # Fin de ton code
 
 
