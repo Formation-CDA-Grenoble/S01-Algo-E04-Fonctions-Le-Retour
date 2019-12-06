fb8faeeef2aaf993f60b6bb4c1d5b96ed9933168
Romain Tello <romain.tello@gmail.com>
2019-12-06 16:07:21 +0100

    Solve exercise 5

--- exo5.py
+++ exo5.py
@@ -1,6 +1,20 @@
+import random
+
 def mix(list):
     # Début de ton code
-    pass
+
+    # Initialise la liste à renvoyer
+    result = []
+    # Tant que la liste d'origine n'est pas vide
+    while len(list) > 0:
+        # Prend un élément au hasard de la liste d'origine
+        index = random.randint(0, len(list) - 1)
+        # Met l'élément dans la liste à renvoyer
+        result.append(list[index])
+        # Supprimer l'élément de la liste d'origine
+        del list[index]
+    # Renvoie la liste
+    return result
     # Fin de ton code
 
 
